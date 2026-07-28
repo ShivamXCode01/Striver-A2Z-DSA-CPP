d9f11f65cf40289a2795d352a532284e5e526892
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-07-28 18:47:56 +0530

    Day-1

---
+++ 01_Learn_the_Basics/01_Things_to_Know_in_CPP/02_CPP_Basics.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 10;
+    float b = 20.5;
+    double c = 30.56789;
+    char d = 'A';
+    bool e = true;
+
+    cout << "Integer : " << a << endl;
+    cout << "Float   : " << b << endl;
+    cout << "Double  : " << c << endl;
+    cout << "Character : " << d << endl;
+    cout << "Boolean : " << e << endl;
+
+    return 0;
+}
