58f7cce4454ec63d3715bde09722819683542a95
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-07-28 19:29:50 +0530

    Day-1

---
+++ 01_Learn_the_Basics/01_Things_to_Know_in_CPP/08_Funcations_a_.cpp
@@ -0,0 +1,19 @@
+// This is an example of call by value funcation 
+
+#include <iostream>
+using namespace std;
+
+// Funcations to add two numbers 
+void Add(int n1 , int n2){
+    cout << "Addition of two numbers is : " << n1+n2;
+}
+
+int main(){
+    int n1,n2;
+    cout << "Enter 1st number : ";
+    cin >> n1;
+    cout <<"Enter 2nd number : ";
+    cin >>n2;
+
+    Add(n1,n2);
+}
