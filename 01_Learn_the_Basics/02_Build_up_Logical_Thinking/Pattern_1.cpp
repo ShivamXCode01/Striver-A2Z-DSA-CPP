353e491246eecf4b65c4023d899bddab7035d24b
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-07-28 20:13:37 +0530

    Day-2

--- 01_Learn_the_Basics/02_Build_up_Logical_Thinking/Pattern_1.cpp
+++ 01_Learn_the_Basics/02_Build_up_Logical_Thinking/Pattern_1.cpp
@@ -1,6 +1,24 @@
+// Pattern:- 1 We want to print 
+//  *  *  *  * 
+//  *  *  *  * 
+//  *  *  *  * 
+//  *  *  *  * 
+
 #include <iostream>
 using namespace std;
 
 int main(){
-    
+    int n ;
+    cout << "Enter the size of square pattern : ";
+    cin >> n ;
+
+    for (int i = 0 ; i < n ; i++){
+        for (int j = 0 ; j < n ; j++){
+            cout << " * ";
+            
+        }
+        cout << endl ;
+        
+    }
+    return 0 ;
 }
