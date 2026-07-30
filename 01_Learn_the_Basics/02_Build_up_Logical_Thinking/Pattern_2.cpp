d7fcd91624f13449b2ed97111567aef1b91864aa
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-07-30 17:26:02 +0530

    Day-3

---
+++ 01_Learn_the_Basics/02_Build_up_Logical_Thinking/Pattern_2.cpp
@@ -0,0 +1,21 @@
+// Pattern:- 1 We want to print
+//  * 
+//  *  * 
+//  *  *  * 
+//  *  *  *  * 
+//  *  *  *  *  * 
+# include <iostream>
+using namespace std;
+
+int main (){
+    int n ;
+    cout << "Enter the height : ";
+    cin >> n ;
+    
+    for (int i = 0 ; i < n ;i++){
+        for (int j = 0 ; j <= i; j++){
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
