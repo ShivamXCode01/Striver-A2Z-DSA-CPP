731e8a24dbba03940fa864ba1b3a935e0a58b34a
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-07-30 20:15:54 +0530

    Day-3

---
+++ 01_Learn_the_Basics/02_Build_up_Logical_Thinking/Pattern_13.cpp
@@ -0,0 +1,29 @@
+// Pattern:- 10 We want to print
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
+// 11 12 13 14 15
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     int n ;
+    cout << "Enter the height : ";
+    cin >> n ;
+
+
+  int num = 1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+    cout <<endl;
+
+}
