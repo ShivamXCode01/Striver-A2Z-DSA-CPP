d48b5952d2b21025e3fc032acc7c45b2e81a47cf
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-01 12:07:44 +0530

    Day-4

---
+++ 01_Learn_the_Basics/03_Pattern/Pattern_3.cpp
@@ -0,0 +1,21 @@
+// Pattern:- 3 We want to print
+//  1 
+//  1  2 
+//  1  2  3 
+//  1  2  3  4 
+//  1  2  3  4  5 
+# include <iostream>
+using namespace std;
+
+int main (){
+    int n ;
+    cout << "Enter the height : ";
+    cin >> n ;
+    
+    for (int i = 1 ; i < n ;i++){
+        for (int j = 1 ; j <= i; j++){
+            cout <<" "<<j<<" ";
+        }
+        cout << endl;
+    }
+}
