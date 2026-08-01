3d02dea98eaca978eda30745b5f14ae0e8130a3c
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-01 11:38:43 +0530

    Day-4

---
+++ 01_Learn_the_Basics/02_Build_up_Logical_Thinking/Pattern_20.cpp
@@ -0,0 +1,38 @@
+// Pattern:- 20 We want to print
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     int n ;
+    cout << "Enter the height : ";
+    cin >> n ;
+
+    for (int i = 0 ; i < n ; i++){
+        for (int j = 0 ; j <= i ; j++){
+            cout <<"*";
+        }
+        for (int j = 2 ; j < 2*(n-i); j++){
+            cout << " ";
+        }
+        for (int j = 0 ; j <= i ; j++){
+            cout <<"*";
+        }
+        cout << endl;
+        
+    }
+    for (int i = 1 ; i < n ; i++){
+        for (int j = 0 ; j <n-i ; j++){
+            cout << "*";
+        }
+        for (int j = 0; j < 2*i; j++){
+            cout <<" ";
+        }
+        for (int j = 0 ; j <n-i ; j++){
+            cout << "*";
+        }
+        cout <<endl;
+    }
+}
