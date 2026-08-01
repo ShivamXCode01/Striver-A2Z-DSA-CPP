34ddd18c8ab1aa124fd141845f741d1b8302b4d5
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-01 12:16:27 +0530

    Day-4

---
+++ 01_Learn_the_Basics/03_Pattern/Pattern_17.cpp
@@ -0,0 +1,49 @@
+// Pattern:- 17 We want to print
+//     A    
+//    ABA   
+//   ABCBA  
+//  ABCDCBA 
+// ABCDEDCBA
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     int N ;
+    cout << "Enter the height : ";
+    cin >> N ;
+
+    for (int i = 0; i < N; i++) {
+
+        // Print leading spaces
+        for (int j = 0; j < N - i - 1; j++) {
+            cout << " ";
+        }
+
+        // Initialize character to start from 'A'
+        char ch = 'A';
+
+        // Calculate midpoint of the row
+        int breakpoint = (2 * i + 1) / 2;
+
+        // Print the characters in the row
+        for (int j = 1; j <= 2 * i + 1; j++) {
+            cout << ch;
+
+            // Increment character till the midpoint, then decrement
+            if (j <= breakpoint) ch++;
+            else ch--;
+        }
+
+        // Print trailing spaces
+        for (int j = 0; j < N - i - 1; j++) {
+            cout << " ";
+        }
+
+        // Newline after each row
+        cout << endl;
+    }
+    
+
+}
